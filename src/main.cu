ebe7971cabf4dfdcc1fa877722288e9808e0f377
Dominik <schdomin@gmail.com>
2013-10-21 00:24:42 +0200

    updated

--- src/main.cu
+++ src/main.cu
@@ -1,57 +1,81 @@
 #include "CCubicDomain.cuh"           //ds domain structure
 #include <iostream>                   //ds cout
+#include <time.h>                     //ds timing
 #include <cuda_runtime.h>             //ds needed for eclipse indexer
 #include <device_launch_parameters.h> //ds needed for eclipse indexer
 
-#define N 20
 
 
-//ds GPU kernels
-__global__ void updateAcceleration( float p_arrAccelerations[100][3] )
+//ds kernels
+__global__ void updateParticlesVelocityVerlet( float* arrPositions, float* arrVelocities, float* arrAccelerations )
 {
-    p_arrAccelerations[0][0] += 1.0;
-    p_arrAccelerations[0][1] += 1.0;
-    p_arrAccelerations[0][2] += 1.0;
+    //ds particle index equals thread index
+    unsigned int uIndex = threadIdx.x;
+
+    //ds get the current structures and save them in vector elements (only for readability)
+    float* vecPosition     = &arrPositions[3*uIndex];
+    float* vecVelocity     = &arrVelocities[3*uIndex];
+    float* vecAcceleration = &arrAccelerations[3*uIndex];
+
+    //ds do calculations
+    vecPosition[0] += 1;
+    vecPosition[1] += 2;
+    vecPosition[2] += 3;
+
+    vecVelocity[0] += 1;
+    vecVelocity[1] += 1;
+    vecVelocity[2] += 1;
+
+    vecAcceleration[0] += 1;
+    vecAcceleration[1] += 1;
+    vecAcceleration[2] += 1;
 }
 
-int main()
+int main( int argc, char** argv )
 {
     //ds domain configuration
     const std::pair< double, double > pairBoundaries( -1.0, 1.0 );
     const unsigned int uNumberOfParticles( 100 );
 
-    //ds host side information (float used since CUDA does not like double)
-    float h_arrPositions[100][3];     //= new float*[uNumberOfParticles];
-    float h_arrVelocities[100][3];    //= new float*[uNumberOfParticles];
-    float h_arrAccelerations[100][3]; // = new float*[uNumberOfParticles];
+    //ds host information
+    float h_arrPositions[uNumberOfParticles][3];
+    float h_arrVelocities[uNumberOfParticles][3];
+    float h_arrAccelerations[uNumberOfParticles][3];
 
-    //ds allocate and set values correctly
-    for( unsigned int u = 0; u < 100; ++u )
+    //ds set values
+    for( unsigned int u = 0; u < uNumberOfParticles; ++u )
     {
-        //ds initialize positions
-        h_arrPositions[u][0] = 0.0;
-        h_arrPositions[u][1] = 0.0;
-        h_arrPositions[u][2] = 0.0;
+        h_arrPositions[u][0] = 0;
+        h_arrPositions[u][1] = 0;
+        h_arrPositions[u][2] = 0;
 
-        //ds initialize velocities
-        h_arrPositions[u][0] = 0.0;
-        h_arrVelocities[u][1] = 0.0;
-        h_arrVelocities[u][2] = 0.0;
+        h_arrVelocities[u][0] = 0;
+        h_arrVelocities[u][1] = 0;
+        h_arrVelocities[u][2] = 0;
 
-        //ds initialize
-        h_arrAccelerations[u][0] = 0.0;
-        h_arrAccelerations[u][1] = 0.0;
-        h_arrAccelerations[u][2] = 0.0;
+        h_arrAccelerations[u][0] = 0;
+        h_arrAccelerations[u][1] = 0;
+        h_arrAccelerations[u][2] = 0;
     }
 
-    //ds accelerations on device
-    float d_arrAccelerations[uNumberOfParticles][3];
+    //ds device handles
+    float *d_arrPositions     ( 0 );
+    float *d_arrVelocities    ( 0 );
+    float *d_arrAccelerations ( 0 );
 
-    //ds allocate memory
-    cudaMalloc( (void**)d_arrAccelerations, 100*3*sizeof( float ) );
+    //ds allocate memory (here we see the advantage of using single pointers instead doubles)
+    cudaMalloc( (void **)&d_arrPositions    , uNumberOfParticles * 3 * sizeof( float ) );
+    cudaMalloc( (void **)&d_arrVelocities   , uNumberOfParticles * 3 * sizeof( float ) );
+    cudaMalloc( (void **)&d_arrAccelerations, uNumberOfParticles * 3 * sizeof( float ) ) ;
 
-    //ds copy memory to host
-    cudaMemcpy( d_arrAccelerations, h_arrAccelerations, uNumberOfParticles*3*sizeof( float ), cudaMemcpyHostToDevice );
+    //ds copy memory to gpu
+    cudaMemcpy( d_arrPositions    , h_arrPositions    , uNumberOfParticles * 3 * sizeof( float ), cudaMemcpyHostToDevice );
+    cudaMemcpy( d_arrVelocities   , h_arrVelocities   , uNumberOfParticles * 3 * sizeof( float ), cudaMemcpyHostToDevice );
+    cudaMemcpy( d_arrAccelerations, h_arrAccelerations, uNumberOfParticles * 3 * sizeof( float ), cudaMemcpyHostToDevice );
+
+    std::cout << "position[0][0]: " << h_arrPositions[0][0] << std::endl;
+    std::cout << "position[0][1]: " << h_arrPositions[0][1] << std::endl;
+    std::cout << "position[0][2]: " << h_arrPositions[0][2] << std::endl;
 
     //ds current simulation configuration
     //const double dTimeStepSize( 0.0001 );
@@ -62,109 +86,30 @@ int main()
     //ds start simulation
     for( unsigned int uCurrentTimeStep = 0; uCurrentTimeStep < uNumberOfTimeSteps; ++uCurrentTimeStep )
     {
-        //ds call the kernel
-        updateAcceleration<<< 1, uNumberOfParticles >>>( d_arrAccelerations );
+        //ds execute timestep -> 1 block: one thread for each particle
+        updateParticlesVelocityVerlet<<< 1, uNumberOfParticles >>>( d_arrPositions, d_arrVelocities, d_arrAccelerations );
 
-        //ds copy the information back from the GPU to the CPU
-        cudaMemcpy( h_arrAccelerations, d_arrAccelerations, uNumberOfParticles*3*sizeof( float ), cudaMemcpyDeviceToHost );
+        //ds get the memory from gpu to cpu
+        cudaMemcpy( h_arrPositions    , d_arrPositions    , uNumberOfParticles * 3 * sizeof( float ), cudaMemcpyDeviceToHost );
+        cudaMemcpy( h_arrVelocities   , d_arrVelocities   , uNumberOfParticles * 3 * sizeof( float ), cudaMemcpyDeviceToHost );
+        cudaMemcpy( h_arrAccelerations, d_arrAccelerations, uNumberOfParticles * 3 * sizeof( float ), cudaMemcpyDeviceToHost );
 
-        //ds print
-        std::cout << h_arrAccelerations[0][0] << std::endl;
-        std::cout << h_arrAccelerations[0][1] << std::endl;
-        std::cout << h_arrAccelerations[0][2] << std::endl;
+        std::cout << "position[0][0]: " << h_arrPositions[0][0] << std::endl;
+        std::cout << "position[0][1]: " << h_arrPositions[0][1] << std::endl;
+        std::cout << "position[0][2]: " << h_arrPositions[0][2] << std::endl;
+        std::cout << "position[1][0]: " << h_arrPositions[1][0] << std::endl;
+        std::cout << "position[1][1]: " << h_arrPositions[1][1] << std::endl;
+        std::cout << "position[1][2]: " << h_arrPositions[1][2] << std::endl;
+        std::cout << "position[2][0]: " << h_arrPositions[2][0] << std::endl;
+        std::cout << "position[2][1]: " << h_arrPositions[2][1] << std::endl;
+        std::cout << "position[2][2]: " << h_arrPositions[2][2] << std::endl;
     }
 
-
-
-    /*ds host information
-    float h_fTest[3];
-
-    h_fTest[0] = 1;
-    h_fTest[1] = 2;
-    h_fTest[2] = 3;
-
-    //ds device information
-    float* d_fTest;
-
-    //ds allocate memory
-    cudaMalloc( (void**)&d_fTest, 3*sizeof( float ) );
-
-    //ds copy memory to host
-    cudaMemcpy( d_fTest, h_fTest, 3*sizeof( float ), cudaMemcpyHostToDevice );
-
-    //ds call the kernel
-    updatePosition<<< 1, 3 >>>( d_fTest );
-
-    //ds copy the information back from the GPU to the CPU
-    cudaMemcpy( h_fTest, d_fTest, 3*sizeof( float ), cudaMemcpyDeviceToHost );
-
-    //ds free the memory
-    cudaFree( d_fTest );
-
-    std::cout << h_fTest[0] << std::endl;
-    std::cout << h_fTest[1] << std::endl;
-    std::cout << h_fTest[2] << std::endl;*/
-
-
-    /*ds current positions
-    int h_iPositionX;
-    int h_iPositionY;
-    int h_iPositionZ;
-
-    //ds device positions
-    int* d_iPositionX;
-    int* d_iPositionY;
-    int* d_iPositionZ;
-
-    //ds allocate the memory on the host
-    d_iPositionX = new int;
-    d_iPositionY = new int;
-    d_iPositionZ = new int;
-
-    //ds allocate the memory on the GPU
-    cudaMalloc( (void**)&d_iPositionX, N*sizeof( int ) );
-    cudaMalloc( (void**)&d_iPositionY, N*sizeof( int ) );
-    cudaMalloc( (void**)&d_iPositionZ, N*sizeof( int ) );
-
-    //ds set the memory layout
-    cudaMemset( d_iPositionX, 0, N );
-    cudaMemset( d_iPositionY, 0, N );
-    cudaMemset( d_iPositionZ, 0, N );
-
-    //ds call the GPU kernels and compute
-    kernel1<<< 1, N >>>( d_iPositionX );
-    kernel2<<< 1, N >>>( d_iPositionY );
-    kernel3<<< 1, N >>>( d_iPositionZ );
-
-    //ds copy the information back from the GPU to the CPU
-    cudaMemcpy( &h_iPositionX, d_iPositionX, N*sizeof(int), cudaMemcpyDeviceToHost );
-    cudaMemcpy( &h_iPositionY, d_iPositionY, N*sizeof(int), cudaMemcpyDeviceToHost );
-    cudaMemcpy( &h_iPositionZ, d_iPositionZ, N*sizeof(int), cudaMemcpyDeviceToHost );
-
-    // display the results
-    std::cout << " Results from kernel1:" << std::endl;
-    for (int i = 0; i<N; i++)
-        std::cout<< h_iPositionX[i] << " ";
-    std::cout<< std::endl;
-
-    std::cout << " Results from kernel2:" << std::endl;
-    for (int i = 0; i<N; i++)
-        std::cout<< h_iPositionY[i] << " ";
-    std::cout<< std::endl;
-
-    std::cout << " Results from kernel3:" << std::endl;
-    for (int i = 0; i<N; i++)
-        std::cout<< h_iPositionZ[i] << " ";
-    std::cout << std::endl;
-
-
-    //free the memory allocated on the GPU
-    cudaFree(d_pa);
-    cudaFree(d_pb);
-    cudaFree(d_pc);*/
+    cudaFree( d_arrPositions );
+    cudaFree( d_arrVelocities );
+    cudaFree( d_arrAccelerations );
 
     return 0;
-
 }
 
 /*
